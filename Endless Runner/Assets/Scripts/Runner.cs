6a08cb9aa92abe055b6ce88f54daa6bdbc435740
STH <sth6197@gmail.com>
2024-09-10 19:47:15 +0900

    (2024.09.10) 게임 리소스 제작 및 배치

---
+++ Endless Runner/Assets/Scripts/Runner.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public enum RoadLine
+{
+    LEFT = -1,
+    MIDDLE,
+    RIGHT
+}
+
+public class Runner : MonoBehaviour
+{
+    [SerializeField] RoadLine roadline;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
