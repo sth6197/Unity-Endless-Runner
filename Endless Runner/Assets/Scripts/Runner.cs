c33319e18ca47b2cd909308f830d71324d0ed21a
STH <sth6197@gmail.com>
2024-09-11 19:46:29 +0900

    (2024.09.11) 캐릭터 이동 시스템

--- Endless Runner/Assets/Scripts/Runner.cs
+++ Endless Runner/Assets/Scripts/Runner.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 
@@ -13,15 +15,58 @@ public enum RoadLine
 public class Runner : MonoBehaviour
 {
     [SerializeField] RoadLine roadline;
+    [SerializeField] Rigidbody rigidBody;
+
+    [SerializeField] float speed = 25.0f;
+    [SerializeField] float positionX = 2.5f;
+
+
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
 
     void Start()
     {
-        
+        roadline = RoadLine.MIDDLE;
     }
 
-    
+    void OnkeyUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if(roadline != RoadLine.LEFT)
+            {
+                roadline--;
+            }
+        }
+        
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if(roadline != RoadLine.RIGHT)
+            {
+                roadline++;
+            }
+        }
+    }
+
     void Update()
     {
-        
+        OnkeyUpdate();   
+    }
+
+    private void Move()
+    {
+        rigidBody.position = Vector3.Lerp
+        (
+            rigidBody.position, 
+            new Vector3((int)roadline * positionX, 0, 0), 
+            speed * Time.fixedDeltaTime
+        );
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
     }
 }
