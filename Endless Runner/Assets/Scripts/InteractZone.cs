9d75e5f20147725e62586c5c28595526e1f3dc4e
STH <sth6197@gmail.com>
2024-09-19 19:46:31 +0900

    (2024.09.19) 도로 관리자 시스템

---
+++ Endless Runner/Assets/Scripts/InteractZone.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractZone : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Endless Runner/Assets/Scripts/RoadManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class RoadManager : MonoBehaviour
+{
+    [SerializeField] List<GameObject> roads;
+    [SerializeField] float speed = 5.0f;
+    
+
+    void Start()
+    {
+        roads.Capacity = 10;
+    }
+
+    
+    void Update()
+    {
+        for (int i = 0; i < roads.Count; i++)
+        {
+            roads[i].transform.Translate(Vector3.back * speed * Time.deltaTime);
+        }
+    }
+}
