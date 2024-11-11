9b73a0b83e0080db5ca4ec008ec9bd4ca52f70be
STH <sth6197@gmail.com>
2024-11-11 16:44:44 +0900

    수정1

--- Endless Runner/Assets/Scripts/Managers/EventManager.cs
+++ Endless Runner/Assets/Scripts/Managers/EventManager.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
-using UnityEditor.Experimental.GraphView;
-using UnityEditor.Search;
 using UnityEngine;
 using UnityEngine.Events;
 
